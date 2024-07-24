dfd4358803c6a8251bd68b09d246b07d5f95f45c
Your Name <asmitastha2001@gmail.com>
2024-07-24 14:19:46 +0545

    feat: perform query and resolver in a fake json

---
+++ fakedb.js
@@ -0,0 +1,34 @@
+
+
+export const users =[
+  {
+    id:'231',
+    firstName:"Asmita",
+    lastName:"Shrestha",
+    email:"asm@gmmail.com",
+    password:'12345',
+  },
+  {
+    id:'431',
+    firstName:"Ashok",
+    lastName:"Shrestha",
+    email:"ashok@gmmail.com",
+    password:'123456',
+  },
+]
+
+
+export const quotes = [
+  {
+    name:"I turn coffee into code",
+    by:'231',
+  },
+  {
+    name:"I turn coffee into code",
+    by:'231',
+  },
+  {
+    name:"If it works don't touch it",
+    by:'431',
+  },
+]
---
+++ server.js
@@ -0,0 +1,45 @@
+import { ApolloServer, gql } from "apollo-server";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { users, quotes } from "./fakedb.js";
+
+const typeDefs = gql`
+  type Query {
+    users: [User]
+    user(id: ID!): User
+    quotes: [Quote]
+  }
+  type User {
+    id: ID!
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    quotes: [Quote]
+  }
+
+  type Quote {
+    name: String
+    by: ID
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: () => users,
+    user:(_,{id})=> users.find(user=> user.id === id),
+    quotes: () => quotes,
+  },
+  User: {
+    quotes: (user) => quotes.filter((quote) => quote.by === user.id),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is running at ${url}`);
+});
